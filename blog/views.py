fbcbc8093a396a45a63f43029395499824d1f3d9
hangogohan595 <hangogohan595@gmail.com>
2024-10-03 12:43:26 +0800

    added initial templates

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+
+def posts(request):
+    return render(request, 'blog/posts.html')
+
+
+def post(request, slug):
+    return render(request, 'blog/post.html')
